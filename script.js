97162762560442e6ed529b73e529ed08d3a97ca8
Felipe de Paula <emilcyfelipe@gmail.com>
2021-09-11 00:01:52 -0300

    themes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let keys = document.querySelectorAll('.key');
 const display = document.querySelectorAll('.displayScreen');
 console.log(display)
 
-//Add the event click to all keys and call the function that will insert the value at the display
+//Add the event click to all keys and insert the values at display
 keys.forEach(key=>{
     key.addEventListener(('click'),()=>{
         if(key.id==='reset'){
@@ -21,6 +21,31 @@ keys.forEach(key=>{
         }
     })
 })
+
+let selectTheme = document.querySelector('#theme');
+selectTheme.addEventListener("change",()=>{
+    themeChange(selectTheme.value)
+})
+
+let themeObject = document.documentElement.style
+
+function themeChange(theme){
+        themeObject.setProperty('--main-background', themes[theme].mainbackground);
+        themeObject.setProperty('--toggle-background', themes[theme].toogleBackground);
+        themeObject.setProperty('--keypad-background', themes[theme].keypadBackground);
+        themeObject.setProperty('--screen-background', themes[theme].screenBackground);
+        themeObject.setProperty('--key-background', themes[theme].keyBackground);
+        themeObject.setProperty('--key-shadow', themes[theme].keyShadow);
+        themeObject.setProperty('--key-action-background', themes[theme].keyActionBackground);
+        themeObject.setProperty('--key-shadow-action-background', themes[theme].keyActionShadow);
+        themeObject.setProperty('--key-background-secondary', themes[theme].keyBackgroundSecondary);
+        themeObject.setProperty('--key-background-secondary-shadow', themes[theme].keyBackgroundShadowSecondary);
+        themeObject.setProperty('--text-primary-color', themes[theme].textPrimary);
+        themeObject.setProperty('--text-secondary-color', themes[theme].textSecondary);
+        themeObject.setProperty('--text-sub', themes[theme].textSub);
+}
+console.log(selectTheme.value);
+
  
 
 //function insert elements on display
---
+++ themes.js
@@ -0,0 +1,50 @@
+var themes=[
+    {
+        'mainbackground': "hsl(222, 26%, 31%)",
+        'toogleBackground':'hsl(223, 31%, 20%)',
+        'keypadBackground':'hsl(223, 31%, 20%)',
+        'screenBackground':'hsl(224, 36%, 15%)',
+        'keyBackground':'hsl(225, 21%, 49%)',
+        'keyShadow':'hsl(224, 28%, 35%)',
+        'keyActionBackground':'hsl(6, 63%, 50%)',
+        'keyToogle':'hsl(6, 63%, 50%)',
+        'keyActionShadow':'hsl(6, 70%, 34%)',
+        'keyBackgroundSecondary':'hsl(45, 7%, 89%)',
+        'keyBackgroundShadowSecondary':'hsl(35, 11%, 61%)',
+        'textPrimary':'hsl(221, 14%, 31%)',
+        'textSecondary':'hsl(0,0%,100%)',
+        'textSub':'hsl(0,0%,100%)'
+    },
+    {
+        'mainbackground': "hsl(0, 0%, 90%)",
+        'toogleBackground':'hsl(0, 5%, 81%)',
+        'keypadBackground':'hsl(0, 5%, 81%)',
+        'screenBackground':'hsl(0, 0%, 93%)',
+        'keyBackground':'hsl(185, 42%, 37%)',
+        'keyShadow':'hsl(185, 58%, 25%)',
+        'keyActionBackground':'hsl(25, 98%, 40%)',
+        'keyToogle':'hsl(25, 98%, 40%)',
+        'keyActionShadow':'hsl(25, 99%, 27%)',
+        'keyBackgroundSecondary':'hsl(45, 7%, 89%)',
+        'keyBackgroundShadowSecondary':'hsl(35, 11%, 61%)',
+        'textPrimary':'hsl(60, 10%, 19%)',
+        'textSecondary':'hsl(0, 0%, 100%)',
+        'textSub':'hsl(60, 10%, 19%)'
+    },
+    {
+        'mainbackground': "hsl(268, 75%, 9%)",
+        'toogleBackground':'hsl(268, 71%, 12%)',
+        'keypadBackground':'hsl(268, 71%, 12%)',
+        'screenBackground':'hsl(268, 71%, 12%)',
+        'keyBackground':'hsl(281, 89%, 26%)',
+        'keyShadow':'hsl(285, 91%, 52%)',
+        'keyActionBackground':'hsl(176, 100%, 44%)',
+        'toogle':'hsl(176, 100%, 44%)',
+        'keyActionShadow':'hsl(177, 92%, 70%)',
+        'keyBackgroundSecondary':'hsl(268, 47%, 21%)',
+        'keyBackgroundShadowSecondary':'hsl(290, 70%, 36%)',
+        'textPrimary':'hsl(52, 100%, 62%)',
+        'textSecondary':'hsl(198, 20%, 13%)',
+        'textSub':'hsl(52, 100%, 62%)'
+    }
+]
